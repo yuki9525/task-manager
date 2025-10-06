6a790c4e3e64c4fe6c5ee7b12f525ba5d3ddf41d
中川　優樹 <01062817@CC7785.local>
2025-10-06 17:46:14 +0900

    feat: Configure Ehcache and enable caching

--- src/main/java/com/example/taskmanager/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanager/TaskManagerApplication.java
@@ -2,8 +2,10 @@ package com.example.taskmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching // ここを追加
 public class TaskManagerApplication {
 
 	public static void main(String[] args) {
